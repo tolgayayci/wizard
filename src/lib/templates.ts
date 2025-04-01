7322942985e7ac8802a8903cf8a0eec32058e277
Tolga Yaycı <40897846+tolgayayci@users.noreply.github.com>
2025-04-02 00:17:26 +0300

    fix

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -482,61 +482,203 @@ impl AccessControl {
 //     }
 // }`;
 
-const ETH_BUCHAREST_CODE = `extern crate alloc;
+const ETH_BUCHAREST_CODE = `// This contract is used as the reference via The Wizard on Stylus. It
+// should be changed to compete in the hackathon!
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+extern crate alloc;
 
-// Define some persistent storage using the Solidity ABI.
-// Counter will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+use stylus_sdk::{alloy_primitives::*, prelude::*};
+
+use alloc::{collections::BTreeMap, vec, vec::Vec};
+
+use libbucharesthashing::{immutables::*, prover, prover::Piece};
+
+/* ~~~~~~~~~~~~~~ BOARD IMPLEMENTATION ~~~~~~~~~~~~~~ */
+
+/// Board that this game is played on. Could be of any size. This could
+/// be optimised for gas golfing.
+pub type Board = BTreeMap<u32, (Piece, u32)>;
+
+fn pos_to_xy(row_size: u32, p: u32) -> (u32, u32) {
+    (p % row_size, p / row_size)
+}
+
+fn xy_to_pos(row_size: u32, x: u32, y: u32) -> u32 {
+    y.wrapping_mul(row_size).wrapping_add(x)
+}
+
+fn in_bounds(row_size: u32, x: u32, y: u32) -> bool {
+    x < row_size && y < row_size
+}
+
+// Find the in check threats for the king given, returning the nonces of
+// the threats.
+fn in_check_threats(board: &Board, row_size: u32, king_pos: u32) -> Vec<u32> {
+    let (king_x, king_y) = pos_to_xy(row_size, king_pos);
+    let mut threats = vec![];
+    // The following code takes the position of the king, then searches for pieces in
+    // positions that might threaten the king, then adding them as threats if they're
+    // the kind of piece to be a threat.
+    macro_rules! piece_add_threat_if_valid {
+        ($piece:ident, $x:expr, $y:expr) => {
+            if in_bounds(row_size, $x, $y) {
+                if let Some((Piece::$piece, n)) = board.get(&xy_to_pos(row_size, $x, $y)) {
+                    threats.push(*n);
+                }
+            }
+        };
+    }
+    // Pawn
+    for dx in [-1, 1] {
+        let x = king_x.wrapping_add_signed(dx);
+        let y = king_y.wrapping_sub(1);
+        piece_add_threat_if_valid!(PAWN, x, y);
+    }
+    // Knight
+    for (dx, dy) in [
+        (-2, -1),
+        (-2, 1),
+        (-1, -2),
+        (-1, 2),
+        (1, -2),
+        (1, 2),
+        (2, -1),
+        (2, 1),
+    ] {
+        piece_add_threat_if_valid!(
+            KNIGHT,
+            king_x.wrapping_add_signed(dx),
+            king_y.wrapping_add_signed(dy)
+        );
+    }
+    // Rook/Queen
+    for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let mut x = king_x;
+        let mut y = king_y;
+        loop {
+            x = x.wrapping_add_signed(dx);
+            y = y.wrapping_add_signed(dy);
+            // It's true that the macro does this check as well, but any compiler
+            // would optimise this out, so we leave it for brevity reasons.
+            if !in_bounds(row_size, x, y) {
+                break;
+            }
+            piece_add_threat_if_valid!(CASTLE, x, y);
+            piece_add_threat_if_valid!(QUEEN, x, y);
+        }
+    }
+    // Bishop/Queen
+    for (dx, dy) in [(-1, -1), (-1, 1), (1, -1), (1, 1)] {
+        let mut x = king_x;
+        let mut y = king_y;
+        loop {
+            x = x.wrapping_add_signed(dx);
+            y = y.wrapping_add_signed(dy);
+            if !in_bounds(row_size, x, y) {
+                break;
+            }
+            piece_add_threat_if_valid!(BISHOP, x, y);
+            piece_add_threat_if_valid!(QUEEN, x, y);
+        }
+    }
+    // King
+    for dx in [-1, 0, 1] {
+        for dy in [-1, 0, 1] {
+            // Make sure we're not hcecking the king against itself, and that we're
+            // not in the corner.
+            let x = king_x.wrapping_add_signed(dx);
+            let y = king_y.wrapping_add_signed(dy);
+            if dx == 0 && dy == 0 || xy_to_pos(row_size, x, y) == king_pos {
+                continue;
+            }
+            piece_add_threat_if_valid!(KING, x, y);
+        }
+    }
+    threats
+}
+
+pub fn solve(starting_hash: &[u8], start: u32) -> Option<(u32, u32)> {
+    let row_size = BOARD_SIZE.isqrt();
+    let mut board = BTreeMap::new();
+    let mut last_king = None;
+    for i in start..MAX_TRIES {
+        let e = prover::hash(starting_hash, i);
+        let king_id: u8 = Piece::KING.into();
+        let p_id: u8 = (e % (king_id as u64 + 1)).try_into().unwrap();
+        let p = Piece::try_from(p_id).unwrap();
+        let offset: u32 = (e >> 32).try_into().unwrap();
+        let pos: u32 = offset % BOARD_SIZE;
+        board.insert(pos, (p, i));
+        if p == Piece::KING {
+            last_king = Some((pos, i));
+        }
+        if let Some((last_king_pos, last_king_nonce)) = last_king {
+            let mut threats = in_check_threats(&board, row_size, last_king_pos);
+            if threats.len() >= CHECKS_NEEDED as usize {
+                threats.push(last_king_nonce);
+                return Some((*threats.iter().min().unwrap(), i));
+            }
+        }
+    }
+    None
+}
+
+/* ~~~~~~~~~~~~~~ CONTRACT ENTRYPOINT ~~~~~~~~~~~~~~ */
+
+#[storage]
+#[entrypoint]
+pub struct Storage {}
+
+#[public]
+impl Storage {
+    // We need to provide this function for the prover contract to check this
+    // contract's performance with this function.
+    pub fn prove(&self, hash: FixedBytes<32>, from: u32) -> Result<(u32, u32), Vec<u8>> {
+        Ok(solve(hash.as_slice(), from).unwrap())
     }
 }
 
-/// Declare that Counter is a contract with the following external methods.
-#[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
+/* ~~~~~~~~~~~~~~ ALGORITHM TESTING ~~~~~~~~~~~~~~ */
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
+// This test code will randomly slam the function to test if it behaves
+// consistently. It will create hashes for the test function.
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments \`number\` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+    proptest! {
+        #[test]
+        fn test_solve(starting_hash in any::<[u8; 64]>()) {
+            // First, let's test if the user-defined algorithm is consistent.
+            let (e_l, e_h) = solve(&starting_hash, 0).unwrap();
+            // Let's run our function against the first invocation of the function!
+            let (t_l, t_h) = solve(&starting_hash, e_l).unwrap();
+            // Now let's check if it's consistent.
+            assert_eq!((e_l, e_h), (t_l, t_h), "user contract not consistent. {e_l} != {t_l} or {e_h} != {t_h}");
+            // Now, let's test if the remote contract's prove function is consistent with the
+            // local function here.
+            let (p_l, p_h) = prover::default_solve(&starting_hash, e_l).unwrap();
+            assert_eq!(
+                (e_l, e_h), (p_l, p_h),
+                "user contract inconsistent with reference. {e_l} != {p_l} or {e_h} != {p_h}"
+            );
+        }
     }
 }`;
 
 export const PROJECT_TEMPLATES = [
   {
-    name: "Welcome to ETH Bucharest",
-    description: "Starter template for ETH Bucharest from bayge.xyz.",
+    name: "Arbitrum Hackathon Gas Redux challenge",
+    description: "Starter template for ETH Bucharest Gas Redux Challenge by Stylus Saturdays and Superposition",
     icon: Code2,
     code: ETH_BUCHAREST_CODE,
     features: [
-      "Get",
-      "Ready",
-      "To",
-      "Hack",
+      "Optimise a Proof of Work function",
+      "Learn Stylus skills",
+      "Compete for $6,800 USD and special keyboards",
+      "Be the champion",
     ],
   },
   {
